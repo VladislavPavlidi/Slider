8a9a4825aea1d3adcf6f2121b9a6d585a94cefcb
VladislavPavlidi <57861040+VladislavPavlidi@users.noreply.github.com>
2021-08-14 22:53:52 +0300

    add fill of the scrollbar

--- dev/res/js-dev/components/views/slider/index.js
+++ dev/res/js-dev/components/views/slider/index.js
@@ -1,8 +1,12 @@
+const SLIDES_PER_SCREEN = 4;
+
 let prev;
 let next;
 let sliderContent;
 let parent;
 let slides;
+let scrollbar;
+let scrollbarFill;
 
 let instance;
 
@@ -12,6 +16,8 @@ function findElements() {
   next = parent.querySelector('.slider__navigation--next');
   sliderContent = parent.querySelector('.slider__content');
   slides = [...parent.querySelectorAll('.slide')];
+  scrollbar = parent.querySelector('.slider__scrollbar');
+  scrollbarFill = scrollbar.querySelector('.slider__scrollbar-fill');
 }
 
 function translateSlider(translation) {
@@ -26,15 +32,23 @@ function isLastSlide() {
   return instance.activeIndex === slides.length - 5;
 }
 
+function setScrollbarFill() {
+  scrollbarFill.style = `width: ${
+    (instance.activeIndex * 100) / (slides.length - SLIDES_PER_SCREEN)
+  }%`;
+}
+
 function onPrevClick() {
   const { scroll, width, activeIndex } = instance;
   if (isFirstSlide()) {
     prev.disabled = true;
   }
-  instance.scroll = scroll + (width / 4 + 40 / 4);
+  instance.scroll =
+    scroll + (width / SLIDES_PER_SCREEN + 40 / SLIDES_PER_SCREEN);
   instance.activeIndex = activeIndex - 1;
   translateSlider(instance.scroll);
   next.disabled = false;
+  setScrollbarFill();
 }
 
 function onNextClick() {
@@ -42,10 +56,12 @@ function onNextClick() {
   if (isLastSlide()) {
     next.disabled = true;
   }
-  instance.scroll = scroll - (width / 4 + 40 / 4);
+  instance.scroll =
+    scroll - (width / SLIDES_PER_SCREEN + 40 / SLIDES_PER_SCREEN);
   instance.activeIndex = activeIndex + 1;
   translateSlider(instance.scroll);
   prev.disabled = false;
+  setScrollbarFill();
 }
 
 function initInstance() {
@@ -55,6 +71,7 @@ function initInstance() {
     activeIndex: 0,
     width: sliderContent.offsetWidth,
     slidesCount: slides.length - 1,
+    scrollbarWidth: 0,
   };
 }
 
